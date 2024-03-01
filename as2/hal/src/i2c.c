fe212fc9d04428f79189f6404372fc490fbe6f03
teun <teun@sfu.ca>
2024-02-29 20:53:17 -0800

    Update i2c.c

--- as2/hal/src/i2c.c
+++ as2/hal/src/i2c.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "hal/i2c.h"
 
 #define GPIO_Config_1 "config-pin P9_18 i2c"
